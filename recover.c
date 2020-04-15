cf33d279984818f332a80a0c83ace912813fa5fb
kardandon <kardandon@users.noreply.github.com>
2020-04-15 21:41:00 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <cs50.h>
 #include <string.h>
+#include <stdint.h>
+
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -16,13 +19,13 @@ int main(int argc, char *argv[])
         printf("Could not open\n");
         return 1;
     }
-    unsigned char *block = malloc(512);
-    int a=1;
+    BYTE block[512];
+    int a = 0;
     FILE *img;
     string filename = "";
     while(!feof(f))
     {
-        fread(block, sizeof(unsigned char), 512, f);
+        fread(block, sizeof(BYTE), 512, f);
         if(block[0]==0xff || block[1] == 0xd8 || block[2] == 0xff || (block[3] & 0xf0) == 0xe0)
         {
             sprintf(filename, "%03i.jpg", a);
@@ -36,7 +39,7 @@ int main(int argc, char *argv[])
         }
         if(img!=NULL)
         {
-            fwrite(block, sizeof(unsigned char), 512, img);
+            fwrite(block, sizeof(BYTE), 512, img);
         }
     }
     return 0;
