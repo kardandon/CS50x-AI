309df836ce0d0cea890aa223e8434b60e0fe7609
kardandon <kardandon@users.noreply.github.com>
2020-04-15 18:16:49 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -129,7 +129,6 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
-    pair_count = 0;
     for (int i = 0 ; i < candidate_count ; i++)
     {
         for (int j = i+1 ; j < candidate_count ; j++)
