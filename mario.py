f5c079bd002659268846cb0dba360e168e1bbb50
kardandon <kardandon@users.noreply.github.com>
2020-04-15 22:37:43 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,22 +1,22 @@
 def main():
-        try:
-            n = int(input("Height: "))
-        except:
-            main()
-            return 0
-        if (n > 8 or n < 1):
-            main()
-            return 0
-        for i in range(n):
-            string = ""
-            for j in range(n - i - 1):
-                string += " "
-            for j in range(i + 1):
-                string += "#"
-            string += "  "
-            for j in range(i + 1):
-                string += "#"
-            print(string)
+    try:
+        n = int(input("Height: "))
+    except:
+        main()
+        return 0
+    if (n > 8 or n < 1):
+        main()
+        return 0
+    for i in range(n):
+        string = ""
+        for j in range(n - i - 1):
+            string += " "
+        for j in range(i + 1):
+            string += "#"
+        string += "  "
+        for j in range(i + 1):
+            string += "#"
+        print(string)
 
-if (__name__ == "__main__"):
-    main()
+
+main()
