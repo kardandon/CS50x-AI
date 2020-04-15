5354f2d993eac68ac4fa0fd0cf5c99812a6a9d20
kardandon <kardandon@users.noreply.github.com>
2020-04-15 13:36:09 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -6,14 +6,18 @@ int main(void)
 int n,i,j;
 printf("Height: ");
 scanf("%d",&n);
+if(n>0){
 for(i=0;i<n;i++){
     for(j=n-i-1;j>0;j--)
         printf(" ");
     for(j=0;j<i+1;j++)
         printf("#");
-    printf(" ");
+    printf("  ");
     for(j=0;j<i+1;j++)
         printf("#");
     printf("\n");
 }
+}
+else printf("You must enter an integer greater than 0;");
+return 0;
 }
