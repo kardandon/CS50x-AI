debc9ce4631cb6f5ec0f5bd20fb222661cc308aa
kardandon <kardandon@users.noreply.github.com>
2020-04-15 13:46:54 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -3,20 +3,29 @@
 
 int main(void)
 {
-int n,i,j;
-do{
-n= get_int("Height: ");
-}while(!(n>0 && n<9));
-for(i=0;i<n;i++){
-    for(j=n-i-1;j>0;j--)
-        printf(" ");
-    for(j=0;j<i+1;j++)
-        printf("#");
-    printf("  ");
-    for(j=0;j<i+1;j++)
-        printf("#");
-    printf("\n");
-}
+    int n, i, j;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (!(n > 0 && n < 9));
+    for (i = 0; i < n; i++)
+    {
+        for (j = n - i - 1; j > 0; j--)
+        {
+            printf(" ");
+        }
+        for (j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (j = 0 ; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 
-return 0;
+    return 0;
 }
